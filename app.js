8af367ac61690ce7ea5a78faeb1d7a376ef68fa6
= <felix.ramirezjr@gmail.com>
2017-05-29 19:54:05 -0700

    Using CORS NPM to allow cross origin access

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var app = express();
 var port = process.env.PORT || 3000;
+var cors = require('cors');
+
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const {OperationHelper} = require('apac');
 
